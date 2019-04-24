4934dbcb00e3aa707c992b8cdfcd55878d3334df
lisniuse <17560235@qq.com>
2019-04-24 18:54:32 +0800

    fix: anaysis controller.

--- app/controller/api/analysis.js
+++ app/controller/api/analysis.js
@@ -26,6 +26,29 @@ class AnalysisController extends Controller {
 			data: logs
 		};
 	}
+
+	/**
+   * @description 统计pv浏览量
+   */
+	async pv() {
+		const { ctx, service, config } = this;
+		if (!ctx.locals.currentUser.auth.isLogin) {
+			return ctx.helper.throwError(ctx, '你没有登陆', 403);
+    }
+    
+    let { startTime, endTime } = ctx.query;
+    startTime = startTime || 0;
+    endTime = endTime || new Date().getTime();
+
+		//获取日志总数
+    const logs = await service.log.findAll({type: 1, createTime: {$gte: startTime, $lt: endTime}});
+
+		ctx.body = {
+			code: 0,
+			msg: '查询成功',
+			data: logs
+		};
+	}
 }
 
 module.exports = AnalysisController;
--- app/routes/api/analysis.js
+++ app/routes/api/analysis.js
@@ -2,4 +2,5 @@
 module.exports = function(app) {
   const { router, controller } = app;
   router.get('/api/analysis/ip', controller.api.analysis.ip); //统计独立ip
+  router.get('/api/analysis/pv', controller.api.analysis.pv); //统计pv浏览量
 }
